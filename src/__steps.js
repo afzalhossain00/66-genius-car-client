c6877177aa34c61cab7c3c90effa7bc51fb9d916
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-11-03 07:23:20 +0600

    first commit

---
+++ src/__steps.js
@@ -0,0 +1,8 @@
+/* 
+Basic Setup:
+1. install react router
+2. tailwind css configuration  ==> npm install -D tailwindcss postcss autoprefixer ==> npx tailwindcss init -p ==> css config
+3. tailwind component library ==> npm i daisyui
+4.
+5.
+*/
