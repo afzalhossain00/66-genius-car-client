c54a98fb8570eb4447c319dff0261fb10889c4a7
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-11-04 19:33:58 +0600

    added sign up page

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
 
-const Login = () => {
+const SignUp = () => {
 
-    const handleLogin = event => {
+    const handleSignUp = event => {
         event.preventDefault()
     }
 
@@ -15,8 +15,14 @@ const Login = () => {
                     <img className='w-3/4' src={img} alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
-                    <h1 className="text-5xl font-bold text-center">Login</h1>
-                    <form onSubmit={handleLogin} className="card-body">
+                    <h1 className="text-5xl font-bold text-center">Sign Up</h1>
+                    <form onSubmit={handleSignUp} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="Your name" className="input input-bordered" />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -25,22 +31,19 @@ const Login = () => {
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Password</span>
+                                <span className="label-text">Confirm Password</span>
                             </label>
                             <input type="password" name='password' placeholder="Your password" className="input input-bordered" required />
-                            <label className="label">
-                                <a href="/" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
                         </div>
                         <div className="form-control mt-6">
-                            <input className="btn btn-primary" type="submit" value="Login" />
+                            <input className="btn btn-primary" type="submit" value="Sign Up" />
                         </div>
                     </form>
-                    <p className='text-center'>New to Genius Car? <Link className='text-orange-600 font-bold' to='/signup'>Sign Up</Link></p>
+                    <p className='text-center'>Already Have an account? <Link className='text-orange-600 font-bold' to='/login'>Login</Link></p>
                 </div>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default SignUp;
