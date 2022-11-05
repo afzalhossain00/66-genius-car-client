c4726b3810b98e2f036c4eaa08de29207afe72bd
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-11-06 03:06:41 +0600

    added private route and delete handler

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -2,10 +2,11 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import OrderRow from './OrderRow';
 
 const Orders = () => {
     const { user } = useContext(AuthContext)
-    const [orders, setOrders] = useState({})
+    const [orders, setOrders] = useState([])
 
     useEffect(() => {
         fetch(`http://localhost:5000/orders?email=${user?.email}`)
@@ -13,9 +14,54 @@ const Orders = () => {
             .then(data => setOrders(data))
     }, [user?.email])
 
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure you want to cancel the order')
+        if (proceed) {
+            fetch(`http://localhost:5000/orders/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        alert('Deleted successfully')
+                        const remaining = orders.filter(ord => ord.id !== id)
+                        setOrders(remaining);
+                    }
+                })
+        }
+    }
+
     return (
         <div>
-            <h2 className="text-4xl">You have {orders.length}</h2>
+            <h2 className="text-4xl">You have {orders.length} Orders</h2>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map(order => <OrderRow
+                                key={order._id}
+                                order={order}
+                                handleDelete={handleDelete}
+                            ></OrderRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
---
+++ src/Router/PrivateRoutes/PrivateRoutes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+
+const PrivateRoutes = ({ children }) => {
+    const { user, loading } = useContext(AuthContext)
+    const location = useLocation();
+
+    if (loading) {
+        return <h2 className="text-4xl">Loading...</h2>
+    }
+
+    if (user) {
+        return children;
+    }
+    return <Navigate state={{ from: location }} replace></Navigate>
+};
+
+export default PrivateRoutes;
