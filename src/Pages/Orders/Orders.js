b44b65ca4b7b75733d41f9bd29ca6445a5869a04
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-11-06 22:41:51 +0600

    update orders

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -25,11 +25,33 @@ const Orders = () => {
                     console.log(data);
                     if (data.deletedCount > 0) {
                         alert('Deleted successfully')
-                        const remaining = orders.filter(ord => ord.id !== id)
+                        const remaining = orders.filter(ord => ord._id !== id)
                         setOrders(remaining);
                     }
                 })
         }
+    }
+
+    const handleStatusUpdate = (id) => {
+        fetch(`http://localhost:5000/orders/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ status: 'Approved' })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    const remaining = orders.filter(odr => odr._id !== id)
+                    const approving = orders.find(odr => odr._id === id)
+                    approving.status = 'Approved'
+
+                    const newOrders = [approving, ...remaining]
+                    setOrders(newOrders)
+                }
+            })
     }
 
     return (
@@ -57,6 +79,7 @@ const Orders = () => {
                                 key={order._id}
                                 order={order}
                                 handleDelete={handleDelete}
+                                handleStatusUpdate={handleStatusUpdate}
                             ></OrderRow>)
                         }
                     </tbody>
